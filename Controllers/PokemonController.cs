23171fa085b301daf76cf831fa56750ac2ba1b18
belemaaa <graceitamunoala@gmail.com>
2024-02-15 11:09:16 +0100

    changed settings

--- Controllers/PokemonController.cs
+++ Controllers/PokemonController.cs
@@ -7,73 +7,73 @@ using PokemonReviewApi.Models;
 
 namespace PokemonReviewApi.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
+    [Route("api/[controller]")]
+    [ApiController]
 
-	public class PokemonController : Controller
-	{
+    public class PokemonController : Controller
+    {
         private readonly IPokemonRepository _pokemonRepository;
         private readonly IMapper _mapper;
 
         public PokemonController(IPokemonRepository pokemonRepository, IMapper mapper)
-		{
+        {
             this._pokemonRepository = pokemonRepository;
             this._mapper = mapper;
         }
 
 
-		[HttpGet]
-		[ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
-		public IActionResult GetPokemons()
-		{
-			var pokemons = _mapper.Map<List<PokemonDto>>(_pokemonRepository.GetPokemons());
-			if (!ModelState.IsValid)
-				return BadRequest(ModelState);
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        public IActionResult GetPokemons()
+        {
+            var pokemons = _mapper.Map<List<PokemonDto>>(_pokemonRepository.GetPokemons());
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
-			return Ok(pokemons);
-		}
+            return Ok(pokemons);
+        }
 
 
-		[HttpGet("{pokemonId}")]
-		[ProducesResponseType(200, Type = typeof(Pokemon))]
-		[ProducesResponseType(400)]
-		[ProducesResponseType(404)]
-		public IActionResult GetPokemonById(int pokemonId)
-		{
+        [HttpGet("{pokemonId}")]
+        [ProducesResponseType(200, Type = typeof(Pokemon))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult GetPokemonById(int pokemonId)
+        {
             bool pokemonExists = _pokemonRepository.PokemonExists(pokemonId);
-			if (!pokemonExists)
-			{
-				return NotFound("Pokemon object not found");
-			}
+            if (!pokemonExists)
+            {
+                return NotFound("Pokemon object not found");
+            }
 
-			var pokemon = _mapper.Map<PokemonDto>(_pokemonRepository.GetPokemon(pokemonId));
+            var pokemon = _mapper.Map<PokemonDto>(_pokemonRepository.GetPokemon(pokemonId));
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
             return Ok(pokemon);
-		}
+        }
 
 
-		[HttpGet("{pokemonId}/rating")]
+        [HttpGet("{pokemonId}/rating")]
         [ProducesResponseType(200, Type = typeof(decimal))]
         [ProducesResponseType(400)]
         [ProducesResponseType(404)]
-		public IActionResult GetPokemonRating(int pokemonId)
-		{
-			bool pokemonExists = _pokemonRepository.PokemonExists(pokemonId);
-			if (!pokemonExists)
-			{
-				return NotFound("Pokemon object not found");
-			}
+        public IActionResult GetPokemonRating(int pokemonId)
+        {
+            bool pokemonExists = _pokemonRepository.PokemonExists(pokemonId);
+            if (!pokemonExists)
+            {
+                return NotFound("Pokemon object not found");
+            }
 
-			var pokemonRating = _pokemonRepository.GetPokemonRating(pokemonId);
-			if (!ModelState.IsValid)
-			{
-				return BadRequest(ModelState);
-			}
-			return Ok(pokemonRating);
-		}
+            var pokemonRating = _pokemonRepository.GetPokemonRating(pokemonId);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(pokemonRating);
+        }
 
     }
 }
